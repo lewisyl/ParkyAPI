4fded72c9c389bd5ac4c2b1ccdb047e99fcf1855
Lewis Liu <lewisliuusa@outlook.com>
2021-05-06 10:38:08 -0700

    added park repo and interface

---
+++ ParkyAPI/Repository/NationalParkRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ParkyAPI.Data;
+using ParkyAPI.Models;
+using ParkyAPI.Repository.IRepository;
+
+namespace ParkyAPI.Repository
+{
+    public class NationalParkRepository : INationalParkRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public NationalParkRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public ICollection<NationalPark> GetNationalParks()
+        {
+            return _db.NationalParks.OrderBy(park => park.Name).ToList();
+        }
+
+        public NationalPark GetNationalPark(int nationalParkId)
+        {
+            return _db.NationalParks.FirstOrDefault(park => park.Id == nationalParkId);
+        }
+
+        public bool NationalParkExists(string name)
+        {
+            var result = _db.NationalParks.Any(park => park.Name.ToLower().Trim() == name.ToLower().Trim());
+            return result;
+        }
+
+        public bool NationalParkExists(int id)
+        {
+            var result = _db.NationalParks.Any(park => park.Id == id);
+            return result;
+        }
+
+        public bool CreateNationalPark(NationalPark nationalPark)
+        {
+            _db.NationalParks.Add(nationalPark);
+            return Save();
+        }
+
+        public bool UpdateNationalPark(NationalPark nationalPark)
+        {
+            _db.NationalParks.Update(nationalPark);
+            return Save();
+        }
+
+        public bool DeleteNationalPark(NationalPark nationalPark)
+        {
+            _db.NationalParks.Remove(nationalPark);
+            return Save();
+        }
+
+        public bool Save()
+        {
+            var result = _db.SaveChanges() > 0 ? true : false;
+            return result;
+        }
+    }
+}
